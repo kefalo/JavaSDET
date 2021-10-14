407e49f0b32f905cf103b6062a921d45313ee2d4
kefalo <stefanmkac@gmail.com>
2021-10-14 15:09:54 +0200

    renaming Variables class adding Operators and covering all until Conditional statements

---
+++ src/main/java/DataTypes/VariablesAndFirstJavaProgram.java
@@ -0,0 +1,27 @@
+package DataTypes;
+
+public class VariablesAndFirstJavaProgram {
+
+	public static void main(String[] args) {
+		//Variables and data types in Java
+		
+		
+		int a = 10; // integer a whole number
+		
+		String s = "stefan"; //sentence, word, text
+		
+		char c = 's'; // only one character
+		
+		double d = 10.10010101; // whole number with up to 16 decimal places
+		
+		boolean b = true;
+		
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(b);
+
+	}
+
+}
