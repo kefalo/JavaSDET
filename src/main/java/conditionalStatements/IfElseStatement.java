dad239bd3824bb092c8afbdafe4a49c49363cd8e
kefalo <stefanmkac@gmail.com>
2021-10-14 17:07:18 +0200

    adding conditional statements classes if ifelse

---
+++ src/main/java/conditionalStatements/IfElseStatement.java
@@ -0,0 +1,27 @@
+package conditionalStatements;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		//Conditional Statements
+		
+		//if..else conditional statements
+		/*
+		 if(condition){
+		 
+		 } else{
+		 
+		 }
+		 */
+		int age = 29;
+		
+		if(age >= 50) {
+			System.out.println("Person is getting mature");
+		} else {
+			System.out.println("Person is too young");
+			System.out.println();
+			}
+		System.out.println("Program exited");
+	}
+
+}
--- src/main/java/conditionalStatements/ifStatement.java
+++ src/main/java/conditionalStatements/ifStatement.java
@@ -3,8 +3,29 @@ package conditionalStatements;
 public class ifStatement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Conditional Statements
+		
+		//if
+		//if..else
+		//nested if..else
+		//switch..case
+		
+		/*
+		 if(condition){
+		 	//code
+		 }
+		 */
+		System.out.println("conditional statements - 'if' statement");
+		
+		int age = 15;
+		System.out.println("person age is " + age);
+		
+		System.out.println("Let's see if person is eligible for vote");
+		
+		if(age <= 18) {
+			System.out.println("Person can vote");
+		}
+		System.out.println("program exited");
 	}
 
 }
